93620e58eb63b298c389b2b347743756cb3ce651
= <122011721+lindnech@users.noreply.github.com>
2023-10-10 10:40:03 +0200

    update

--- main.tf
+++ main.tf
@@ -2,23 +2,23 @@ provider "aws" {
   region = "eu-central-1"  # Setzt die AWS-Region auf "eu-central-1"
 }
 
-############################# Lambda ##################################
+# ############################# Lambda ##################################
 
-resource "aws_lambda_function" "get_driver" {
-  function_name = "getdriverlambda"  # Der Name der Lambda-Funktion
-  role          = aws_iam_role.lambda_exec_role.arn  # Die IAM-Rolle, die der Funktion zugewiesen wird
-  handler       = "index.lambda_handler"  # Der Handler, der aufgerufen wird, wenn die Funktion ausgeführt wird
-  runtime       = "python3.9"  # Die Laufzeitumgebung für die Funktion
+# resource "aws_lambda_function" "get_driver" {
+#   function_name = "getdriverlambda"  # Der Name der Lambda-Funktion
+#   role          = aws_iam_role.lambda_exec_role.arn  # Die IAM-Rolle, die der Funktion zugewiesen wird
+#   handler       = "index.lambda_handler"  # Der Handler, der aufgerufen wird, wenn die Funktion ausgeführt wird
+#   runtime       = "python3.9"  # Die Laufzeitumgebung für die Funktion
 
-  filename = "./getdriver/index.zip"  # Der Pfad zur ZIP-Datei, die den Code der Funktion enthält
-}
+#   filename = "./getdriver/index.zip"  # Der Pfad zur ZIP-Datei, die den Code der Funktion enthält
+# }
 
-resource "aws_lambda_event_source_mapping" "dynamodb_event_source" {
-  event_source_arn = aws_dynamodb_table.OrderDB.stream_arn  # Die ARN des DynamoDB-Streams, der als Ereignisquelle dient
-  function_name = aws_lambda_function.get_driver.arn  # Die ARN der Lambda-Funktion, die aufgerufen wird, wenn ein Ereignis eintritt
-  starting_position          = "LATEST"  # Der Punkt im Stream, an dem die Funktion zu lesen beginnt
-}
-
+# resource "aws_lambda_event_source_mapping" "dynamodb_event_source" {
+#   event_source_arn = aws_dynamodb_table.OrderDB.stream_arn  # Die ARN des DynamoDB-Streams, der als Ereignisquelle dient
+#   function_name = aws_lambda_function.get_driver.arn  # Die ARN der Lambda-Funktion, die aufgerufen wird, wenn ein Ereignis eintritt
+#   starting_position          = "LATEST"  # Der Punkt im Stream, an dem die Funktion zu lesen beginnt
+# }
+# +-
 resource "aws_lambda_function" "orderput" {
   function_name = "orderlambda"  # Der Name der Lambda-Funktion
   role          = aws_iam_role.lambda_exec_role.arn  # Die IAM-Rolle, die der Funktion zugewiesen wird
@@ -79,7 +79,7 @@ resource "aws_iam_policy" "lambda_policy" {
   })
 }
 
-############################ DynamoDB ############################
+# ############################ DynamoDB ############################
 
 resource "aws_dynamodb_table" "OrderDB" {
   name           = "Orders"  # Der Name der Tabelle
@@ -97,26 +97,26 @@ resource "aws_dynamodb_table" "OrderDB" {
  }
 }
 
-# Dieser Code verwendet Terraform, um AWS-Ressourcen zu erstellen und zu verwalten. 
-# Es definiert zwei AWS Lambda-Funktionen (getdriverlambda und orderlambda), eine 
-# DynamoDB-Tabelle (Orders) und eine IAM-Rolle (lambda-exec-role) mit einer zugehörigen Richtlinie (lambda-policy).
+# # Dieser Code verwendet Terraform, um AWS-Ressourcen zu erstellen und zu verwalten. 
+# # Es definiert zwei AWS Lambda-Funktionen (getdriverlambda und orderlambda), eine 
+# # DynamoDB-Tabelle (Orders) und eine IAM-Rolle (lambda-exec-role) mit einer zugehörigen Richtlinie (lambda-policy).
 
-# Die Lambda-Funktionen sind so konfiguriert, dass sie auf Ereignisse reagieren, 
-# die von einem DynamoDB-Stream generiert werden (definiert durch das aws_lambda_event_source_mapping-Ressourcenobjekt).
+# # Die Lambda-Funktionen sind so konfiguriert, dass sie auf Ereignisse reagieren, 
+# # die von einem DynamoDB-Stream generiert werden (definiert durch das aws_lambda_event_source_mapping-Ressourcenobjekt).
 
-# Die IAM-Rolle und -Richtlinie geben den Lambda-Funktionen die erforderlichen 
-# Berechtigungen zum Lesen und Schreiben in DynamoDB und zum Erstellen und Schreiben von Loggruppen und -streams.
+# # Die IAM-Rolle und -Richtlinie geben den Lambda-Funktionen die erforderlichen 
+# # Berechtigungen zum Lesen und Schreiben in DynamoDB und zum Erstellen und Schreiben von Loggruppen und -streams.
 
-# Die DynamoDB-Tabelle ist so konfiguriert, dass sie einen Stream von Änderungen an den 
-# Tabellendaten bereitstellt, auf die die Lambda-Funktionen reagieren können. Der Primärschlüssel der Tabelle ist packageID.
+# # Die DynamoDB-Tabelle ist so konfiguriert, dass sie einen Stream von Änderungen an den 
+# # Tabellendaten bereitstellt, auf die die Lambda-Funktionen reagieren können. Der Primärschlüssel der Tabelle ist packageID.
 
-############################ SQS ############################
+# ############################ SQS ############################
 
-resource "aws_sqs_queue" "DHL-SQS-queue" {
-  name = "DHL-SQS-queue"
-  delay_seconds = 90
-  max_message_size = 2048
-  message_retention_seconds = 86400
-  receive_wait_time_seconds = 10
-}
+# resource "aws_sqs_queue" "DHL-SQS-queue" {
+#   name = "DHL-SQS-queue"
+#   delay_seconds = 90
+#   max_message_size = 2048
+#   message_retention_seconds = 86400
+#   receive_wait_time_seconds = 10
+# }
 
